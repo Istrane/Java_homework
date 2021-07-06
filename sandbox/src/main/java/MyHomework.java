b25573ee8bfcf0bd44f28c09b65ef396d81d2d00
Istrane <86684863+Istrane@users.noreply.github.com>
2021-07-06 21:16:24 +0400

    Создана первая программа

---
+++ sandbox/src/main/java/MyHomework.java
@@ -0,0 +1,12 @@
+public class MyHomework{
+
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Roman");
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+}
